73a92f9f909b65eb210574baf04598fb1d837e09
maxbausher <maxbausher@gmail.com>
2017-05-18 23:48:09 +1000

    Bank homework and today's insult generator... also final version of wordguesser now working and cleaned up

--- max_bausher/week01/arraysHomework/js/wordguesser.js
+++ max_bausher/week01/arraysHomework/js/wordguesser.js
@@ -2,65 +2,66 @@
 //
 // Create two global arrays: one to hold the letters of the word (e.g. 'F', 'O', 'X'), and one to hold the current guessed letters (e.g. it would start with '_', '_', '_' and end with 'F', 'O', 'X').
 
-var lettersOfWord = ['F', 'O', 'X'];
-var guessedLetters = ['', '', ''];
-var reward = [];
-var finalScore;
 
-var rewardCalculator = function() {
-  function getRandomInt(min,max) {
-    console.log("initialize");
-  min = Math.ceil(min);
-  max = Math.floor(max);
+
+var rewardCalculator = function(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-  };
 };
-
-var rewardAnalyzer = function() {
-  for (var i = 0; i < guessedLetters.length; i++) {
-      if (guessedLetters[i].toString == lettersOfWord[i].toString) {
-        reward[i] = (rewardCalculator(1,100));
-        console.log("Great job, you found one and get points!")
-        console.log(reward.toString);
-  } else if (guessedLetters[i].toString != lettersOfWord[i].toString){
-        reward.pop();
-        console.log("Sorry, bad guess! You lose your last points")
-  } else if (lettersOfWord.toString == guessedLetter.toString){
-        var finalScore = reward.reduce(function(a, b){
-          return a + b; }, 0)
-          console.log("You're taking home the money!");
-        }
-      };
-    };
+//
+// var rewardAnalyzer = function() {
+//   for (var i = 0; i < guessedLetters.length; i++) {
+//       if (guessedLetters[i].toString == lettersOfWord[i].toString) {
+//         reward += 1;
+//         console.log("Great job, you found one and get 1 points!")
+//         // console.log(reward.toString);
+//   } else if (guessedLetters[i].toString != lettersOfWord[i].toString){
+//         reward -= 1;
+//         console.log("Sorry, bad guess! You lose your last points")
+//   } else if (lettersOfWord.toString == guessedLetter.toString){
+//         // var finalScore = reward.reduce(function(a, b){
+//         //   return a + b; }, 0)
+//           console.log("You're taking home the money! Which is " + reward + "dollars");
+//         }
+//       };
+//     };
 
 // Write a function called guessLetter that will:
 // Take one argument, the guessed letter.
 // Iterate through the word letters and see if the guessed letter is in there.
-var guessLetters = function(guess) {
-    for (var i = 0; i < lettersOfWord.length; i++) {
-       if (guess == lettersOfWord[i]){
-         // If the guessed letter matches a word letter, changed the guessed letters array to reflect that.
-         guessedLetters[i] = guess;
-         // When it's done iterating, it should log the current guessed letters ('F__') and congratulate the user if they found a new letter.
-         console.log(guessedLetters);
-          alert("You guessed one, keep going!");
-          rewardAnalyzer();
-         // It should also figure out if there are any more letters that need to be guessed, and if not, it should congratulate the user for winning the game.
-           if (lettersOfWord.toString() == guessedLetters.toString()){
-              alert("You got it!");
-          } else {
-            console.log("No match, keep trying!");
-          }
-        }
-      };
-    };
 
+var lettersOfWord = ['F', 'O', 'X'];
+var guessedLetters = ['_', '_', '_'];
+var reward = 0;
+
+
+
+// var guessLetters = function(guess) {
+//     var letterMatch = false;
+//     for (var i = 0; i < lettersOfWord.length; i++) {
+//
+//        if (guess === lettersOfWord[i]){
+//             var letterMatch = true;
+//          // If the guessed letter matches a word letter, changed the guessed letters array to reflect that.
+//             guessedLetters[i] = guess;
+//             reward += 1;
+//          // When it's done iterating, it should log the current guessed letters ('F__') and congratulate the user if they found a new letter.
+//             console.log(guessedLetters);
+//             console.log("You guessed one, keep going!");
+//
+//          // It should also figure out if there are any more letters that need to be guessed, and if not, it should congratulate the user for winning the game.
+//        } else /*(guess != lettersOfWord[i])*/ {
+//             i = lettersOfWord.length
+//             console.log("No match, keep trying!");
+//             reward -= 1;
+//           }
+//         // } if (lettersOfWord.toString == guessedLetters.toString){
+//         //     console.log("You're taking home the money! Which is " + reward + "dollars");
+//         // }
+//       }
+//     };
+//
 
 // Pretend you don't know the word, and call guessLetter multiple times with various letters to check that your program works.
-guessLetters("M");
-guessLetters("F");
-guessLetters("O");
-guessLetters("X");
 
 // Bonus: Make it more like Wheel of Fortune:
 //
@@ -68,7 +69,43 @@ guessLetters("X");
 // Every time a letter is guessed, generate a random amount and reward the user if they found a letter (multiplying the reward if multiple letters found), otherwise subtract from their reward.
 // When they guess the word, log their final reward amount.
 
+var guessLetters = function(guess) {
 
+    var letterMatch = false;
+    for (var i = 0; i < lettersOfWord.length; i++) {
+
+      console.log('guess:' + guess + ', lettersOfWord[' + i + ']:' + lettersOfWord[i] );
+      console.log('guess === lettersOfWord[i]', guess === lettersOfWord[i]);
+       if (guess === lettersOfWord[i]){
+            var letterMatch = true;
+         // If the guessed letter matches a word letter, changed the guessed letters array to reflect that.
+            guessedLetters[i] = guess;
+            reward += rewardCalculator(1,10);
+         // When it's done iterating, it should log the current guessed letters ('F__') and congratulate the user if they found a new letter.
+            console.log(guessedLetters);
+            console.log("You guessed one, keep going!");
+
+         // It should also figure out if there are any more letters that need to be guessed, and if not, it should congratulate the user for winning the game.
+
+       }
+
+      } // for
+
+      if( letterMatch === false ){
+         console.log("No match for " + guess + ", keep trying!");
+         reward -= rewardCalculator(1,10);
+       }
+
+      if (lettersOfWord.toString() == guessedLetters.toString()){
+          console.log("You're taking home the money! Which is " + reward + " dollars");
+      }
+
+    };
+
+    guessLetters("F");
+    guessLetters("M");
+    guessLetters("O");
+    guessLetters("X");
 
 
 // Bonus: Make it like Hangman:
---
+++ max_bausher/week01/bankHomework/js/bank.js
@@ -0,0 +1,123 @@
+// JavaScript Bank
+//
+// In this homework, you'll create a basic bank in Javascript. The bank has many accounts and the following capabilities that you need to write.
+//
+// Bank
+//
+// There is only one bank. This bank has an array of accounts. The bank needs a method that will return the total sum of money in the accounts. It also needs an addAccount method that will enroll a new account at the bank and add it to the array of accounts. There is no need to create additional functions of the bank to delete accounts, etc.
+//
+// The bank has many accounts. Accounts should be objects that all share a set of common functionality.
+//
+// Accounts
+//
+// Accounts have a current balance and owner's name. You should be able to deposit or withdraw from an account to change the balance.
+//
+// There is no need to write a user interface. Make sure functions return values -- you may also have your functions console.log() values to help you see your code working.
+//
+// You should write a basic story through a series of JavaScript commands that shows that the methods do indeed work as expected: add some accounts, show the total balance, make some deposits and withdrawals, show the new total balance.
+//
+// Tips
+//
+// Don't overthink this. Shorter code is probably the answer.
+//
+// Bonus
+//
+// Ensure that the accounts cannot have negative values.
+// Write a 'transfer' on the bank that allows you to transfer amounts between two accounts.
+// Additional
+
+
+
+var accounts = [
+    {
+      ownerName: "Max Bausher",
+      currentBalance: 1000,
+      password: "shasta"
+    },
+    {
+      ownerName: "Luke Hammer",
+      currentBalance: 4000,
+      password: "wdi22ismyfavoriteclass"
+    }
+  ];
+
+// console.log(accounts);
+
+// var depositAmount;
+// var withdarawlAmount;
+
+// var viewNewTotal = function (password) {
+//   var sum = 0;
+//   for (var i = 0; i < accounts.length; i++) {
+//       if (password = accounts[i].password){
+//         sum += accounts.currentBalance;
+//         console.log(sum);
+//       }
+//     }
+//   alert("Your new balance is " + sum + "!");
+// };
+
+var makeDeposit = function (depositAmount, account) {
+  console.log("===>" + depositAmount, account);
+  account.currentBalance += parseInt(depositAmount);
+  alert("Current balance is now: " + account.currentBalance);
+  // console.log("This account has been received a deposit of " + account.currentBalance);
+};
+
+var makeWithdrawl = function (withdarawlAmount, account) {
+  account.currentBalance -= parseInt(withdarawlAmount);
+  alert("Current balance is now: " + account.currentBalance);
+  // console.log("===> This account has been received a deposit of " + account.currentBalance)
+  // console.log("viewCurrentBalance is alive!")
+};
+
+var viewCurrentBalance = function(password) {
+  for (var i = 0; i < accounts.length; i++) {
+      if (password === accounts[i].password){
+        var res =  prompt("Welcome back " + accounts[i].ownerName + ", your current balance is " + accounts[i].currentBalance + ". Would you like to make a withdrawl or deposit?");
+        console.log(res);
+            if (res === "deposit"){
+            var depositAmount = prompt("Thank you for your business, how much would you like to deposit today?");
+            console.log(depositAmount, accounts[i]);
+            makeDeposit(depositAmount, accounts[i]);
+
+        };
+        if (res === "withdrawl"){
+          var withdarawlAmount = prompt("Cash money!!!! How much would you like to take out this time?");
+          console.log(withdarawlAmount);
+           makeWithdrawl(withdarawlAmount, accounts[i]);
+        }
+      };
+  }
+  // console.log("viewCurrentBalance is alive!")
+};
+
+var createNewAccount = function () {
+  var newCustomerName = prompt("Please provide your name: ");
+  var initialDepositAmount = prompt("Great! We will be sending you an e-mail verification after your intial deposit. How much will you deposit this time?");
+  var newPassword = prompt("One last step! Set your super secret password");
+  var newAccount = {
+    ownerName: newCustomerName,
+    currentBalance: parseInt(initialDepositAmount),
+    password: newPassword
+  }
+  accounts.push(newAccount);
+  alert("Success! Your account is now set up. Please check your e-mail for verification. Thank you picking Bada$$ Bank!")
+  // console.log(newAccount);
+};
+
+var promptStart = function () {
+      var response = prompt('Welcome to Bada$$ Bank, if you are an existing user, please supply your password. If you want to start banking with us, respond: new customer');
+        if (response === "new customer") {
+          createNewAccount();
+        } else {
+          viewCurrentBalance(response);
+        }
+    };
+
+
+promptStart();
+// viewCurrentBalance(1);
+// viewNewTotal();
+// viewCurrentBalance(2);
+// viewNewTotal();
