632ef36ab306cac4b75e73da0506da87fe2cdc49
Michelle Erica <michelle.erica.lo@gmail.com>
2017-05-21 22:44:46 +1000

    progress

--- michelle_lo/week01/MTA/js/2updatedObjecttoPlaywith.js
+++ michelle_lo/week01/MTA/js/2updatedObjecttoPlaywith.js
@@ -9,42 +9,89 @@ var metroLines = {
   line6: ["Grand Central", "33rd", "28th", "23rd", "Union Square","Astor Place"]
 };
 
-var planTrip = function(startLine, startStation, destLine, destStation) {{
-  var startStationsVar = metroLines[ startLine ];
-    console.log("TEST A " + startStationsVar + " this is test of var stations = metroLines[ startLine ]");
-    for (var i = 0; i < startStationsVar.length; i++) {
-        if (startStation === startStationsVar[i])
-        console.log(startStationsVar[i] + " is on " + [startLine])}
-        var pureStartIndex = metroLines[startLine].indexOf(startStation)
-        var startIndex = (pureStartIndex + 1) //+ 1 so you dont rprint the start station
-        console.log (startIndex);
-    // if (startStation === metroLines[startLine][i])
+var planTrip = function(startLine, startStation, destLine, destStation) {
+  var change = "";
+  if (startLine === destLine){
+    var startStationsVar = metroLines[ startLine ];
+      console.log("TEST A " + startStationsVar + " this is test of var stations = metroLines[ startLine ]");
+      for (var i = 0; i < startStationsVar.length; i++) {
+          if (startStation === startStationsVar[i])
+          console.log(startStationsVar[i] + " is on " + [startLine])}
+          var pureStartIndex = metroLines[startLine].indexOf(startStation)
+          var startIndex = (pureStartIndex + 1) //+ 1 so you dont rprint the start station
+          console.log ("Index number " + pureStartIndex + " of " + startLine);
 
-  };
+    var destStationsVar = metroLines[ destLine ];
+      console.log("TEST B " + destStationsVar + " this is test of var stations = metroLines[ destLine ]");
+      for (var i = 0; i < destStationsVar.length; i++) {
+          if (destStation === destStationsVar[i])
+          console.log(destStationsVar[i] + " is on " + [destLine])}
+          var pureDestIndex = metroLines[destLine].indexOf(destStation);
+          var destIndex = (metroLines[destLine].indexOf(destStation) - 2) //-1 so you dont reprint the end station
+          console.log ("Index number " + pureDestIndex + " of " + destLine);
 
+    {
+      var listOfStationsPassed = startStationsVar.splice([startIndex],[destIndex])
+      console.log("You must travel through the following stops on the " + startLine + ": " + listOfStationsPassed)
+      var numStops = pureDestIndex- pureStartIndex;
+      console.log("There are " + numStops + " in total")
+    }  // if (startStation === metroLines[startLine][i])
+  } else { change = "Change at Union Square"
+    { //first part of journey start -> Union Square
+      var startStationsVar = metroLines[ startLine ];
+      console.log("TEST A2: CHANGING TRAINS " + startStationsVar + " this is test of var stations = metroLines[ startLine ]");
+      for (var i = 0; i < startStationsVar.length; i++) {
+          if (startStation === startStationsVar[i])
+          console.log(startStationsVar[i] + " is on " + [startLine])}
+          var pureStartIndex = metroLines[startLine].indexOf(startStation)
+          var startIndex = (pureStartIndex + 1) //+ 1 so you dont rprint the start station
+          console.log ("Index number " + pureStartIndex + " of " + startLine);
 
-  {
-  var destStationsVar = metroLines[ destLine ];
-    console.log("TEST B " + destStationsVar + " this is test of var stations = metroLines[ startLine ]");
-    for (var i = 0; i < destStationsVar.length; i++) {
-        if (destStation === destStationsVar[i])
-        console.log(destStationsVar[i] + " is on " + [destLine])}
-        var pureDestIndex = metroLines[destLine].indexOf(destStation);
-        var destIndex = (metroLines[destLine].indexOf(destStation) - 2) //-1 so you dont reprint the end station
-        console.log (destIndex);
+    var destStationsVarForUS = metroLines[ startLine ];
+      console.log("TEST B2: Changing Trains " + destStationsVarForUS + " this is test of var stations = metroLines[ destLine ]");
+      for (var i = 0; i < destStationsVarForUS.length; i++) {
+          if ("Union Square" === destStationsVarForUS[i])
+          console.log(destStationsVarForUS[i] + " is on " + [startLine])}
+          var pureDestIndex = metroLines[startLine].indexOf("Union Square");
+          var destIndex = (metroLines[startLine].indexOf("UnionSqare") - 2) //-1 so you dont reprint the end station
+          console.log ("Index number " + pureDestIndex + " of " + startLine);
+    }
+    //Second part of journey: Union Square - destStation
+     var startStationsVarforUS = metroLines[ startLine ];
+     console.log("TEST A3: CHANGING TRAINS " + startStationsVarforUS + " this is test of var stations = metroLines[ startLine ] when changing trains");
+     for (var i = 0; i < startStationsVarforUS.length; i++) {
+         if ("Union Square" === startStationsVarforUS[i])
+         console.log(startStationsVarforUS[i] + " is on " + [destLine])}
+         var pureStartIndex = metroLines[destLine].indexOf("Union Square")
+         var startIndex = (pureStartIndex + 1) //+ 1 so you dont rprint the start station
+         console.log ("Index number " + pureStartIndex + " of " + destLine);
+
+   var destStationsVar = metroLines[ destLine ];
+     console.log("TEST B " + destStationsVar + " this is test of var stations = metroLines[ destLine ]");
+     for (var i = 0; i < destStationsVar.length; i++) {
+         if (destStation === destStationsVar[i])
+         console.log(destStationsVar[i] + " is on " + [destLine])}
+         var pureDestIndex = metroLines[destLine].indexOf(destStation);
+         var destIndex = (metroLines[destLine].indexOf(destStation) - 2) //-1 so you dont reprint the end station
+         console.log ("Index number " + pureDestIndex + " of " + destLine);
+
+    {
+      var listOfStationsPassed = startStationsVar.splice([startIndex],[destIndex])
+      console.log("You must travel through the following stops on the " + startLine + ": " + listOfStationsPassed)
+      var numStops = pureDestIndex- pureStartIndex;
+      console.log("There are " + numStops + " in total")
+    }  // if (startStation === metroLines[startLine][i])
   }
-  {
-    var listOfStationsPassed = startStationsVar.splice([startIndex],[destIndex])
-    console.log("You must travel through the following stops on the " + startLine + ": " + listOfStationsPassed)
-    var numStops = pureDestIndex- pureStartIndex;
-    console.log("There are " + numStops + " in total")
-  }  // if (startStation === metroLines[startLine][i])
+  console.log ("You will start on " + startLine + " at " + startStation + ". " + change + "" + destLine + " and disembark at " + destStation)
+
 }
+
+
 //
 // var listOfStationsPassed = metroLines.lineN.slice(1,3)
 // console.log(listOfStations)
 
-planTrip("lineN", "34th", "lineN", "Union Square");
+planTrip("lineN", "34th", "line6", "28th");
 
 //
 // var findStartStation = function (startLine, startStation ){
