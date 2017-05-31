02fc6ae5cc965d724d12624f913bbc2c75c2677f
Zinh AL-Sweedy <zinh@Zinhs-MacBook-Pro.local>
2017-06-01 09:13:47 +1000

    second week homework

---
+++ zin_al-sweedy/week02/explain/ex.js
@@ -0,0 +1,17 @@
+// week2 notes
+  //kb tricks or shortcuts one hand
+// shortcuts in fils
+// save file cmd+s
+// switch between app cmd+ tab fast or slow to choose one app
+// tab to move bet the app or use the mouse to move faster
+// how find combinations
+// cmd +r to reload
+// cmd +~ to open more than one window open in the atom
+ //cmd +space serach then open in atom
+ // cmd +space serach then open in atom
+ // not highlight the line to copy it
+ // or in terminal open the file by drag it
+
+
+
+//html +css
