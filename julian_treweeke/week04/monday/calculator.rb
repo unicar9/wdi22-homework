9f69cb593789246d3a61bbf560fbe0912c1884a7
“juliantreweeke” <juliantreweeke@gmail.com>
2017-06-05 21:43:53 +1000

    Monday night week 4 homework

--- julian_treweeke/week04/monday/calculator.rb
+++ julian_treweeke/week04/monday/calculator.rb
@@ -17,37 +17,6 @@
 # Advanced Calculator functionality
 # Calculator should be able to do basic arithmetic (exponents, square roots)
 
-# puts "Welcome to the Calculator"
-# puts "Menu of operations"
-# puts "+ ,- ,* , /"
-# puts "First number"
-# first_num = gets.to_i
-# puts "operator"
-# first_op = gets.chomp.to_i
-# puts "Second number"
-# second_num = gets.to_i
-# result = (first_num+first_op+second_num)
-# puts "= #{result}"
-
-# Bonus
-#
-# Mortgage Calculator
-#
-# Calculate the monthly required payment given the other variables as input (look up the necessary variables)
-#
-# BMI Calculator
-#
-# Calculate the body mass index (BMI) for an individual, given their height and weight
-#
-# Trip Calculator
-#
-# Calculate a trip time and cost given inputs for
-#
-# distance
-# miles per gallon
-# price per gallon
-# speed in miles per hour
-
 
 # calculator
 
@@ -105,31 +74,20 @@ puts "result = #{result}"
 
 
 
-# Mortgage Calculator unfinished!
+# Mortgage Calculator
 
+puts "Mortgage Calculator!"
+puts "What is the loan amount?"
+loan_amount = gets.to_f
+puts "Monthly interest rate?"
+monthly_rate = gets.to_f
+monthly_rate /= 12
+puts "How many years to pay off the loan?"
+years = gets.to_f
+years *=  12
+result = loan_amount * ( ( monthly_rate * ( ( 1 + monthly_rate )**180 ) )/( ( ( 1 + monthly_rate )**180 ) -1 ) )
+puts "Your monthly repayment is #{ result }"
 
-# P = monthly payment
-# L = Loan amount
-# c = monthly interest rate. This is the annual interest rate divided by 12.
-# n = number of months in the loan (years * 12)
-#
-# puts "Mortgage Calculator!"
-# "What is the loan amount?"
-# loan_amount = gets.to_i
-# "Monthly interest rate?"
-# monthly_rate = gets.to_i
-# "Years left in the loan?"
-# number_of_months = get.to_i*12
-# monthly_payment =
-#
-# puts "Mortgage Calculator!"
-# "What is the loan amount?"
-# L = gets.to_i
-# "Monthly interest rate?"
-# c = gets.to_i
-# "Years left in the loan?"
-# n = get.to_i*12
-# monthly_payment =
 
 
 
@@ -146,13 +104,6 @@ puts "Your BMI is #{bmi}"
 
 
 # Trip Calculator
-#
-# Calculate a trip time and cost given inputs for
-#
-# distance
-# kms per gallon
-# price per gallon
-# speed in miles per hour
 
 puts "Distance of trip in kms?"
 distance = gets.to_i
@@ -168,88 +119,3 @@ puts "Your trip will take #{trip_time} hours"
 
 cost = (distance/kms_per_litre)*price_per_litre
 puts "Your trip will cost $#{cost}"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# puts "How many km's is your god damn trip buddy?"
-# kms = gets.to_i
-# puts "Cost of the fuel you got in price per kilometer?"
-# price = gets.to_i
-# puts "How good is your car man? Fuel economy - kilometers per litre"
-# economy = gets.to_i
-# puts "how fast were you going. Km's per hour?"
-# speed = gets.to_i
-#
-# fuelneeded = kms/economy
-# tripcost = fuelneeded*price
-# puts "It will take you #{fuelneeded} litres to complete your trip."
-# puts "It will cost you $#{tripcost} to do this god damn trip buddy."
-#
-#
-#
-# cost = distance / per kilmoter * price per litre
-# time = distance / speed
-
-
-
-# Divide the distance by your car's fuel economy (miles per gallon or kilometers per liter). The resulting amount is the number of gallons or liters it will take you to complete your trip. Multiply the number of gallons or liters by the average cost of fuel.
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-
-
-
-
-
-
-
-
-
-#
-# CMath.sqrt(-1)
-
-
-
-
-#
-# puts "First number"
-# first_num = gets.to_i
-# puts "operator"
-# first_op = gets.chomp.to_i
-# puts "Second number"
-# second_num = gets.to_i
-# result = (first_num+first_op+second_num)
-# puts "= #{result}"
