f0c48d09d6fbe8095dbb0b96f0056a3528934415
AliceYunanTang <tangyunan@hotmail.com>
2017-06-06 11:22:51 +1000

    submit week 04 day 1 homework

---
+++ alice_tang/week04/calculator/trip.rb
@@ -0,0 +1,24 @@
+## Trip Calculator
+# Calculate a trip time and cost given inputs for
+# - distance
+# - miles per gallon
+# - price per gallon
+# - speed in miles per hour
+
+puts "distance (miles): "
+distance = gets.to_f
+
+puts "miles per gallon: "
+miles_per_gallon = gets.to_f
+
+puts "price per gallon: "
+price_per_gallon = gets.to_f
+
+puts "speed in miles per hour: "
+speed_in_miles_per_hour = gets.to_f
+
+trip_time = distance / speed_in_miles_per_hour
+cost = distance / miles_per_gallon * price_per_gallon
+
+puts "trip time is #{ trip_time } hours"
+puts "trip cost is #{ cost } dollars"
