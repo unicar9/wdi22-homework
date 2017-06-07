a4f323ae0b6353699663ebe61f00771ef898e3a6
maxbausher <maxbausher@gmail.com>
2017-06-08 00:09:46 +1000

    second commit on movie database homework

--- max_bausher/week04/moviedatabase/app.rb
+++ max_bausher/week04/moviedatabase/app.rb
@@ -8,14 +8,17 @@ get "/" do
 end
 
 get "/movie_details/:id" do
-    url = "https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=" + @results["id"]["overview"]
+
+    url = "https://api.themoviedb.org/3/movie/" + params["id"] + "?api_key=24d863d54c86392e6e1df55b9a328755"
+    # binding.pry
+    @details = HTTParty.get url
     erb :movie_details
 end
 
 get "/show_results" do
     url = "https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=" + params["keyword"]
-    @mdata = HTTParty.get url
-    binding.pry
+    mdata = HTTParty.get url
+    # binding.pry
     @results = mdata["results"]
 
     # @result_count
