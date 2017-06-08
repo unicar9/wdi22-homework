e34980ed7f07b13deadda2789593837340a09132
maxbausher <maxbausher@gmail.com>
2017-06-08 23:06:43 +1000

    mountains homework with active record and a jquery enabled button! needs css bad though

--- max_bausher/week02/cssHomework/cat/js/backup.js
+++ max_bausher/week02/cssHomework/cat/js/backup.js
@@ -4,15 +4,21 @@ console.log('we are here', cat);
 var catMoveIncrement = 10;
 
 window.setInterval(function(){
-  var left = $cat.css('left');   // $cat.css('left');
+  var left = $cat.css('left');
+  // var newleft =
+  // $cat.css('left');
   left = parseInt(left);
   if(left > window.innerWidth - $cat.width()){
     $cat.css('transform', 'scaleX(-1)');
     catMoveIncrement = -10;
+  } else if(left < 230 && catMoveIncrement === -10){ // && !takeOffDone){
+
+      $cat.css('zoomOut')
   } else if(left < 0){
     $cat.css('transform', 'scaleX(1)');
     catMoveIncrement = 10;
   }
+  window.clearInterval();
   $cat.css('left', (left + catMoveIncrement) + 'px');
   // debugger;
 }, 65);
---
+++ max_bausher/week04/nzhikes-sql/public/js/main.js
@@ -0,0 +1,4 @@
+
+$('#list').click(function(){
+   window.location.href='/hikes';
+})
