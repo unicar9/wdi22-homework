a7e362efda5e15d3d9999b173cb7927dd4bd36b5
maxbausher <maxbausher@gmail.com>
2017-06-08 10:11:21 +1000

    final version moviedb

--- max_bausher/week04/moviedatabase/app.rb
+++ max_bausher/week04/moviedatabase/app.rb
@@ -7,20 +7,23 @@ get "/" do
     erb :search
 end
 
+get "/show_results" do
+    if params['keyword'].nil?
+        return "Please supply a search string"
+    end
+    url = "https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=" + params["keyword"]
+    movie_data = HTTParty.get url
+    # binding.pry
+    @results = movie_data["results"]
+
+    # @result_count
+    erb :show_results
+end
+
 get "/movie_details/:id" do
 
     url = "https://api.themoviedb.org/3/movie/" + params["id"] + "?api_key=24d863d54c86392e6e1df55b9a328755"
     # binding.pry
     @details = HTTParty.get url
     erb :movie_details
-end
-
-get "/show_results" do
-    url = "https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=" + params["keyword"]
-    mdata = HTTParty.get url
-    # binding.pry
-    @results = mdata["results"]
-
-    # @result_count
-    erb :show_results
 end
