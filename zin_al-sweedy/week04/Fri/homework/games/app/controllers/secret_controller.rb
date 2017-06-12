cdbb51f8264f7b5740411951373baacea96837a1
Zinh AL-Sweedy <zinh@192-168-1-5.tpgi.com.au>
2017-06-12 20:27:20 +1000

    update and finish my homework for week4

---
+++ zin_al-sweedy/week04/Fri/homework/games/app/controllers/secret_controller.rb
@@ -0,0 +1,20 @@
+class SecretController < ApplicationController
+
+  # single view on /games/secret/
+    # has buttons on page
+
+    # if you click one of thos buttons got to:
+      # /games/secret/4
+
+
+def play
+
+
+ @number = rand(1..10)
+
+
+
+ # raise
+end
+
+end
